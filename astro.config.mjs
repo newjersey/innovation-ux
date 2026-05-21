1a3a5b61de40b5f8b9beff3693faec10d1d8a045
Alex Robinson <alex.robinson@innovation.nj.gov>
2026-05-21 14:41:26 -0400

    Updates to research and overall structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,17 +18,36 @@ export default defineConfig({
 			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/newjersey/innovation-design' }],
 			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				{ label: 'Home', link: '/' },
 				{
 					label: 'Research',
-					autogenerate: { directory: 'research' },
+					items: [
+						{ label: 'Overview', link: '/research/overview/' },
+						{ label: 'Plan research', link: '/research/plan/' },
+						{ label: 'Conduct research', link: '/research/conduct/' },
+						{ label: 'Share findings', link: '/research/share-findings/' },
+						{
+							label: 'Resources',
+							items: [
+								{ label: 'Research resources', link: '/research/resources/' },
+								{ label: '30-minute guide', link: '/research/30-minute-conversation-guide/' },
+								{ label: '60-minute guide', link: '/research/60-minute-conversation-guide/' },
+								{ label: 'Remote research plan', link: '/research/remote-research-plan/' },
+								{ label: 'In-person research plan', link: '/research/in-person-research-plan/' },
+							],
+						},
+					],
+				},
+				{
+					label: 'How we work',
+					items: [
+	                        { label: 'Overview', link: '/how-we-work/overview/' },
+						{ label: 'Content Design & Language Access', link: '/how-we-work/content-design-language-access/' },
+						{ label: 'Design', link: '/how-we-work/design/' },
+						{ label: 'NJWDS', link: '/how-we-work/njwds/' },
+						{ label: 'Enablement', link: '/how-we-work/enablement/' },
+						{ label: 'Leadership', link: '/how-we-work/leadership/' },
+					],
 				},
 			],
 		}),
